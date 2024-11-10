aa026879c5c90a263836cd7fecb46104ae904461
abinesan <31610774+abinesan1@users.noreply.github.com>
2024-11-10 23:59:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
 
-  return "<h1><center> Hello World app! Version 1.2 change!...new relase new<center><h1>"
+  return "<h1><center> Hello World app! Version 1.3 change!...new relase new<center><h1>"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',port=5000)
